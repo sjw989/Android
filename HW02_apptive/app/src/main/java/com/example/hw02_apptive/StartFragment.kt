9ad2e1f9a31d1cd01817dd5f0143c7059223c5f8
sjw989 <a980917a@naver.com>
2022-07-03 22:58:39 +0900

    init

--- HW02_apptive/app/src/main/java/com/example/hw02_apptive/StartFragment.kt
+++ HW02_apptive/app/src/main/java/com/example/hw02_apptive/StartFragment.kt
@@ -1,6 +1,7 @@
 package com.example.hw02_apptive
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -37,6 +38,7 @@ class StartFragment : Fragment() {
 
         binding.btnLogin.setOnClickListener(){
             myViewModel.updateValue(binding.etvID.text.toString(),binding.etvPW.text.toString())
+            Log.d("view : ", myViewModel.id.value.toString())
             findNavController().navigate(R.id.action_startFragment_to_showFragment)
         }
     }
